2df2d6f05fbcca6cadbc6b8ed550a03e4d0afd7f
bethurban <beth.urban@gmail.com>
2019-10-02 09:33:51 -0400

    Solve capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,42 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+//
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//
+//   return result;
+// }
+
+
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(' ');
+}
+
+// function capitalize(str) {
+//   let arr = str.split(' ');
+//   let newWord = '';
+//   let finalStr = '';
+//   for (let word of arr) {
+//     newWord += word[0].toUpperCase();
+//     newWord += word.slice(1);
+//     finalStr += newWord + ' ';
+//     newWord = '';
+//   }
+//   return finalStr.slice(0, finalStr.length - 1);
+// }
 
 module.exports = capitalize;
